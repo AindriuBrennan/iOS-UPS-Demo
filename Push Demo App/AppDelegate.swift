fb4eeb70b4848fc74edba77fe0928bb8303c53c3
Massimiliano Ziccardi <mziccard@redhat.com>
2020-01-28 16:04:54 +0100

    iOS9

--- Push Demo App/AppDelegate.swift
+++ Push Demo App/AppDelegate.swift
@@ -12,7 +12,7 @@ import AeroGearPush
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window : UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -21,12 +21,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -34,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     //register the app with Unified Push Server
-    func application(_ application: UIApplication, didRegisterForRemoteNotifictionsWithDeviceToken deviceToken: Data) {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         
         // setup registration
         let registration = DeviceRegistration(serverURL: URL(string: "http://localhost:9999/")!)
